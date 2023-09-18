1b10af3ca8d897623d382e831956d1db3397b2d4
xemura <markg0s@mail.ru>
2023-09-18 15:15:09 +0300

    add NotesViewModel

--- app/src/main/java/com/xenia/notesapp/di/AppModule.kt
+++ app/src/main/java/com/xenia/notesapp/di/AppModule.kt
@@ -6,6 +6,7 @@ import androidx.room.Room
 import com.xenia.notesapp.feature_note.data.data_source.NoteDatabase
 import com.xenia.notesapp.feature_note.data.repository.NoteRepositoryImpl
 import com.xenia.notesapp.feature_note.domain.repository.NoteRepository
+import com.xenia.notesapp.feature_note.domain.use_case.AddNote
 import com.xenia.notesapp.feature_note.domain.use_case.DeleteNote
 import com.xenia.notesapp.feature_note.domain.use_case.GetNotes
 import com.xenia.notesapp.feature_note.domain.use_case.NoteUseCases
@@ -34,7 +35,8 @@ object AppModule {
     fun provideNoteUseCases(repository: NoteRepository) : NoteUseCases {
         return NoteUseCases(
             getNotes = GetNotes(repository),
-            deleteNote = DeleteNote(repository)
+            deleteNote = DeleteNote(repository),
+            addNote = AddNote(repository)
         )
     }
 }
