531077daa0e8ef512b25eb9aba70261dcd142563
xemura <markg0s@mail.ru>
2023-09-25 19:59:11 +0300

    few changes

--- app/src/main/java/com/xenia/notesapp/feature_note/MainActivity.kt
+++ app/src/main/java/com/xenia/notesapp/feature_note/MainActivity.kt
@@ -68,19 +68,3 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NotesAppTheme {
-        Greeting("Android")
-    }
-}
